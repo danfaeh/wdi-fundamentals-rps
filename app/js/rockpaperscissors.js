2aac3b25f48ec6d6b1a4faaceada827b909a9d84
danfaeh <danfaeh@gmail.com>
2016-01-11 16:57:38 -0800

    playToFive function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,7 +35,6 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-
     if playerMove == computerMove {
         winner = "tie";
     } else if (ComputerMove == 'rock' && playerMove== 'scissors') || (ComputerMove == 'scissors' && playerMove== 'paper') || (ComputerMove == 'paper' && playerMove== 'rock'){
@@ -51,8 +50,20 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    var games = playerWins + computerWins;
+
+        if (playerWins<5 && computerWins<5) {
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+        }
+
     return [playerWins, computerWins];
 }
+
+
+
+
+
+
+
 
